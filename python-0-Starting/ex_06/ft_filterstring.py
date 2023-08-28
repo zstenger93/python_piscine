2decc419554e3430f1aaf79c16d89ce510105059
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-28 16:49:05 +0200

    tester for starting 06 part 1

--- python-0-Starting/ex_06/ft_filterstring.py
+++ python-0-Starting/ex_06/ft_filterstring.py
@@ -1,10 +1,20 @@
 import sys
 
 def main():
+	 """
+    The program accepts two arguments:
+    - A string (S): The input text containing words separated by spaces.
+    - An integer (N): The threshold length for filtering words.
+    
+    The function processes the input text and filters out words whose length is greater
+    than the specified integer N. It then prints the list of filtered words.
+    
+    If the number of arguments is different from 2, or if the type of any argument is wrong,
+    the program prints an AssertionError.
+    """
     try:
         if len(sys.argv) != 3:
             raise AssertionError("Exactly two arguments (string and integer) are required.")
-
         text = sys.argv[1]
         n = int(sys.argv[2])
 
