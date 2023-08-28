f124e321d402298ffa4f659819117efb88f4f1b7
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-28 09:32:33 +0200

    start ex 00

---
+++ python-0-Starting/ex_00/Hello.py
@@ -0,0 +1,26 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+print("\n")
+#your code here
+
+ft_list[1] = "World!"
+
+ft_tuple = ("Hello", "Germany!")
+
+ft_set.remove("tutu!")
+ft_set.add("Heilbronn!")
+
+ft_dict["Hello"] = "42 Heilbronn!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
