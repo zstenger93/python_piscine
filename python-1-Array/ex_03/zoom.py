03c9360851246ec79acdcefa6616e03f2b1af812
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-29 14:48:27 +0200

    array 03 wtf is last printing

---
+++ python-1-Array/ex_03/zoom.py
@@ -0,0 +1,34 @@
+from load_image import ft_load
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import sys
+
+
+def main():
+    try:
+        if image is None:
+            raise AssertionError("Failed to load image.")
+        image = Image.open(sys.argv[1])
+
+        print(ft_load(sys.argv[1]))
+        plt.imshow(image)
+        plt.title("Original Image")
+        plt.axis('on')
+        plt.show()
+
+        zoomed_image = image.crop((400, 100, 800, 600))
+        zoomed_image.save("zoomed_image.jpg")
+        print(f"New shape after cropping: {zoomed_image.size}")
+    
+        plt.imshow(zoomed_image)
+        plt.title("Zoomed Image")
+        plt.axis('on')
+        plt.show()
+
+    except AssertionError as error:
+        print(AssertionError.__name__ + ":", error)
+
+
+if __name__ == "__main__":
+    main()
