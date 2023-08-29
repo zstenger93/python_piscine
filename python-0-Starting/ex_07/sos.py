a8c6252e129f2293e5a84317968afa97c56a7ca0
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-29 08:41:41 +0200

    starting 07 + tester

---
+++ python-0-Starting/ex_07/sos.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def encode_to_morse(text):
+    morse_code_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+        'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+        'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+        '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        ' ': '/',
+    }
+    morse_text = []
+    for char in text.upper():
+        if char in morse_code_dict:
+            morse_text.append(morse_code_dict[char])
+        else:
+            raise AssertionError("Unsupported character: {}".format(char))
+    return ' '.join(morse_text)
+
+
+def main():
+    try:
+        if len(sys.argv) != 2 or len(sys.argv[1]) == 0 or not isinstance(sys.argv[1], str):
+            raise AssertionError("Usage: python3.10 morse_encoder.py <text>")
+        input_text = sys.argv[1]
+        morse_result = encode_to_morse(input_text)
+        print(morse_result)
+    except AssertionError as error:
+        print(error)
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-0-Starting/ex_07/tester.py
@@ -0,0 +1,27 @@
+import subprocess
+
+def test_morse_encoder(input_text, expected_output):
+    try:
+        output = subprocess.check_output(["python3.10", "sos.py", input_text], stderr=subprocess.STDOUT, text=True)
+        assert output.strip() == expected_output.strip()
+        print(f"Test passed: Input: '{input_text}', \nExpected: '{expected_output}', \nOutput: '{output.strip()}'\n")
+    except subprocess.CalledProcessError as e:
+        print(f"Test failed: Input: '{input_text}', \nExpected: '{expected_output}', \nError: {e.output.strip()}\n")
+
+if __name__ == "__main__":
+    test_cases = [
+        ("Hello World", ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."),
+        ("123", ".---- ..--- ...--"),
+        ("Python", ".--. -.-- - .... --- -."),
+        ("%", "Unsupported character: %"),
+        ("Hello?123", "Unsupported character: ?"),
+        ("   ", "/ / /"),
+        ("", "Usage: python3.10 morse_encoder.py <text>"),
+    ]
+    passed_tests = 0
+    for input_text, expected_output in test_cases:
+        test_morse_encoder(input_text, expected_output)
+        print("\033[32m       ok\033[0m")
+        passed_tests += 1
+    print("\033[32m{} out of {} \
+    tests passed\033[0m".format(passed_tests, len(test_cases)))
