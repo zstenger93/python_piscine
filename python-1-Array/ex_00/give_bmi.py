740739a15644444535b3251e1ff47ae7e66d7113
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-29 10:42:55 +0200

    array 00

---
+++ python-1-Array/ex_00/give_bmi.py
@@ -0,0 +1,57 @@
+def give_bmi(
+        height: list[int | float], weight: list[int | float]
+        ) -> list[int | float]:
+    """
+    Calculate BMI values based on given heights and weights.
+
+    Args:
+        height (list[int | float]): List of heights in meters.
+        weight (list[int | float]): List of weights in kilograms.
+
+    Returns:
+        list[int | float]: List of calculated BMI values.
+    """
+    try:
+        if len(height) != len(weight):
+            raise ValueError(
+                "Lists of height and weight must have the same length.")
+        bmi_values = []
+        for h, w in zip(height, weight):
+            if not isinstance(h, (int, float)) \
+                    or not isinstance(w, (int, float)):
+                raise TypeError(
+                    "Height and weight must be integers or floats.")
+            if h <= 0 or w <= 0:
+                raise ValueError("Height and weight values must be positive.")
+            bmi = w / (h ** 2)
+            bmi_values.append(bmi)
+        return bmi_values
+    except Exception as error:
+        print("An error occurred:", error)
+        return []
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    Determine whether BMI values are above a given limit.
+
+    Args:
+        bmi (list[int | float]): List of BMI values.
+        limit (int): Limit to compare BMI values against.
+
+    Returns:
+        list[bool]: List of booleans indicating whether
+        each BMI value is above the limit.
+    """
+    try:
+        if not isinstance(limit, int):
+            raise TypeError("Limit must be an integer.")
+        above_limit = []
+        for value in bmi:
+            if not isinstance(value, (int, float)):
+                raise TypeError("BMI values must be integers or floats.")
+            above_limit.append(value > limit)
+        return above_limit
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
--- python-1-Array/ex_00/tester.py
+++ python-1-Array/ex_00/tester.py
@@ -1,6 +1,11 @@
 from give_bmi import give_bmi, apply_limit
-height = [2.71, 1.15]
-weight = [165.3, 38.4]
+height = [2.71, 1.15, 1.71, 1.15, 1.90]
+weight = [165.3, 38.4, 165.3, 80, 90.5]
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
+
+# If your BMI is less than 18.5, it falls within the underweight range.
+# If your BMI is 18.5 to <25, it falls within the healthy weight range.
+# If your BMI is 25.0 to <30, it falls within the overweight range.
+# If your BMI is 30.0 or higher, it falls within the obesity range.
