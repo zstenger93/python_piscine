c18ee4e44280c56dec0f8631edf5ec258b1fa4ef
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-30 13:31:25 +0200

    OOP 02

--- python-3-OOP/ex_02/DiamondTrap.py
+++ python-3-OOP/ex_02/DiamondTrap.py
@@ -1,1 +1,21 @@
 from S1E7 import Baratheon, Lannister
+
+
+class King(Baratheon, Lannister):
+    def __init__(self, first_name):
+        super().__init__(first_name)
+        self.first_name = first_name
+        self.set_eyes("brown")
+        self.set_hairs("dark")
+
+    def set_eyes(self, color):
+        self.eyes = color
+
+    def set_hairs(self, color):
+        self.hairs = color
+
+    def get_eyes(self):
+        return self.eyes
+
+    def get_hairs(self):
+        return self.hairs
