4922b1bc2eec3928d6b5cdadc2debbddc0f41f74
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-30 10:09:49 +0200

    printing fix

--- python-2-DataTable/ex_03/projection_life.py
+++ python-2-DataTable/ex_03/projection_life.py
@@ -1,14 +1,37 @@
 from load_csv import load
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+
+def format_func(value, tick_number):
+    """
+    formatting the x output to 2k instead of 2000
+    """
+    k_value = int(value) / 1000
+    return f"{k_value:.0f}k"
+
 
 def main():
-    income_data = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+    """
+    This function loads data for Gross National Product (GNP) per capita
+    and life expectancy from separate CSV files. It focuses on data from
+    the year 1900 and visualizes the correlation between GNP and life
+    expectancy through a scatter plot.
+
+    Create a new figure to visualize the scatter plot
+
+    Generate a scatter plot with GNP on
+    the x-axis and life expectancy on the y-axis
+    """
+    certified_42 = "income_per_person_gdppercapita_ppp_inflation_adjusted.csv"
+    income_data = load(certified_42)
     life_expectancy_data = load("life_expectancy_years.csv")
     year_1900_column = '1900'
     gnp_1900 = income_data[year_1900_column]
     life_expectancy_1900 = life_expectancy_data[year_1900_column]
 
     plt.figure(figsize=(10, 6))
+    plt.gca().xaxis.set_major_formatter(mticker.FuncFormatter(format_func))
     plt.scatter(gnp_1900, life_expectancy_1900)
     plt.title("Life expectancy vs Gross domestic product (Year 1900)")
     plt.xlabel("Gross domestic product")
@@ -16,6 +39,7 @@ def main():
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
