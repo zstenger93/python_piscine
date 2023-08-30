514eadb5592cfd68ccc19dd5abfd681340c95963
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-30 13:03:58 +0200

    OOP 02 copied files

---
+++ python-3-OOP/ex_02/S1E7.py
@@ -0,0 +1,42 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name):
+        super().__init__(first_name)
+        self.first_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self):
+        self.is_alive = False
+
+    def __str__(self):
+        return f"Vector: ({self.first_name}, {self.eyes}, {self.hairs})"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Lannister(Character):
+    def __init__(self, first_name):
+        super().__init__(first_name)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self):
+        self.is_alive = False
+
+    def __str__(self):
+        return f"Vector: ({self.first_name}, {self.eyes}, {self.hairs})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive):
+        instance = cls(first_name)
+        instance.is_alive = is_alive
+        return instance
---
+++ python-3-OOP/ex_02/tester.py
@@ -0,0 +1,8 @@
+from DiamondTrap import King
+Joffrey = King("Joffrey")
+print(Joffrey.__dict__)
+Joffrey.set_eyes("blue")
+Joffrey.set_hairs("light")
+print(Joffrey.get_eyes())
+print(Joffrey.get_hairs())
+print(Joffrey.__dict__)
