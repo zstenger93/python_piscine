e9f6284d5535a0e65f2ca21aaa9f110c85406292
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-30 12:40:40 +0200

    OOP 01

--- python-3-OOP/ex_00/S1E9.py
+++ python-3-OOP/ex_00/S1E9.py
@@ -3,38 +3,38 @@ from abc import ABC, abstractmethod
 
 class Character(ABC):
     """Abstract class representing a character."""
-    
+
     def __init__(self, first_name, is_alive=True):
         """
         Constructor for Character.
-        
+
         Parameters:
         first_name (str): The first name of the character.
-        is_alive (bool, optional): The health state of the character. Default is True.
+        is_alive (bool, optional): The health state of the character.
+        Default is True.
         """
         self.first_name = first_name
         self.is_alive = is_alive
-        
+
     @abstractmethod
     def die(self):
         """
         Method to change the health state of the character.
-        
-        This method should be implemented by subclasses to change the health state of
-        the character from alive to dead.
+
+        This method should be implemented by subclasses to change the
+        health state of the character from alive to dead.
         """
         pass
 
 
 class Stark(Character):
     """Class representing a Stark character."""
-    
+
     def die(self):
         """
         Change the health state of the Stark character to False.
-        
-        This method overrides the abstract method from the base class and updates
-        the is_alive attribute to False.
+
+        Overrides the abstract method from the base class and
+        updates the is_alive attribute to False.
         """
         self.is_alive = False
-
