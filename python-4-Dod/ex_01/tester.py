9438056bc69cdd5b2ed5afd50fa6fa36dc762877
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-08-31 10:06:43 +0200

    init dod 01

---
+++ python-4-Dod/ex_01/tester.py
@@ -0,0 +1,14 @@
+from in_out import outer
+from in_out import square
+from in_out import pow
+
+
+my_counter = outer(3, square)
+print(my_counter())
+print(my_counter())
+print(my_counter())
+print("---")
+another_counter = outer(1.5, pow)
+print(another_counter())
+print(another_counter())
+print(another_counter())
