38e840ddb70218343e4fed17be4ef3330731686b
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-09-03 08:11:17 +0200

    whateva

--- python-0-Starting/ex_02/find_ft_type.py
+++ python-0-Starting/ex_02/find_ft_type.py
@@ -11,8 +11,10 @@ def all_thing_is_obj(object: any) -> int:
     type_name = type_names.get(object_type, "Type not found")
 
     if object_type == str:
-        print(f"{object} is in the kitchen : {type_name}")
-    else:
+        print(f"{object} is in the kitchen : {object_type}")
+    elif type_name != "Type not found":
         print(f"{type_name} : {object_type}")
+    else:
+        print(f"{type_name}")
 
     return 42
